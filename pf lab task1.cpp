1993b5af0a26ad111a0ad3328c759b8b8ee4ffed
Z18063 <zukhruffatima48@gmail.com>
2024-10-31 18:34:23 +0500

    Add files via upload

---
+++ pf lab task1.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+#include<string>
+using namespace std;
+int main()
+{
+	string name;
+	string sapid;
+	string semester;
+	string course;
+	cout<<"enter name :";
+	getline(cin,name);
+	cout<<"enter sapid ";
+	getline(cin,sapid);
+	
+	cout<<" enter semester ";
+	getline(cin,semester);
+	cout<<"enter course ";
+	getline(cin,course);
+	cin.get();
+	return 0;
+		
+}
