66d842e5abaaa4b3750b6f89572b49d1cfb56db9
Z18063 <zukhruffatima48@gmail.com>
2024-10-31 18:36:26 +0500

    Add files via upload

---
+++ pf lab task 3.cpp
@@ -0,0 +1,9 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	cout<<"++++++++++++++++++++++++++++++++++++"<<endl;
+	cout<<"WELCOME TO PROGRAMMING FUNDAMENTALS"<<endl;
+		cout<<"++++++++++++++++++++++++++++++++++++"<<endl;
+		return 0;
+}
